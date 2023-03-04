02729a68d005147763650c52d61a79753957d0d3
Alex <102607576+lKoxer@users.noreply.github.com>
2023-03-04 18:28:44 +0100

    Проект на StoryBoard

---
+++ MasterCook1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MasterCook1
+//
+//  Created by Kox on 04.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
